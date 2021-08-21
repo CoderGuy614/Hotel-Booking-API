43b2b57588b872acedc962209b17879a64a3ff6a
CoderGuy614 <jlutz.110@gmail.com>
2021-08-21 18:44:48 -0400

    Create Express Server

---
+++ server.js
@@ -0,0 +1,7 @@
+import express from "express";
+import router from "./routes/auth";
+const app = express();
+
+app.use("/api", router);
+
+app.listen(8000, () => console.log(`Server is running on port 8000`));
