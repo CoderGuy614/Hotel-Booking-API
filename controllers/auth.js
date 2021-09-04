cd28b0476542fd22bfc1db6f21a71bc2e5e44794
CoderGuy614 <jlutz.110@gmail.com>
2021-09-03 20:29:15 -0400

    Create Register User Method and User Schema

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,23 @@
-export const showMessage = (req, res) => {
-  res.status(200).send(req.params.message);
-};
+import User from "../models/user";
 
 export const register = async (req, res) => {
-  console.log("REGISTER");
-  console.log(req.body);
+  const { name, email, password } = req.body;
+  if (!name) return res.status(400).send("Name is required");
+  if (!password || password.length < 6)
+    return res.status(400).send("Password of mininum 6 characters is required");
+  let userExists = await User.findOne({ email }).exec();
+  if (userExists)
+    return res
+      .status(400)
+      .send("Account with this email is already registered");
+
+  const user = new User(req.body);
+  try {
+    await user.save();
+    console.log("USER CREATED", user);
+    return res.json({ ok: true });
+  } catch (error) {
+    console.log("CREATE USER FAILED", err);
+    return res.status(400).send("Something went wrong, please try again");
+  }
 };
